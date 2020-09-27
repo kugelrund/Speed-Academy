0e3e235eaa553e057cab6f2491b2949cb4304ca4
kugelrund <6236137+kugelrund@users.noreply.github.com>
2020-09-27 11:53:57 +0200

    Add special version description for Yavin4 mod
    There are a few issues that were fixed specifically for the
    Escape: Yavin 4 mod. Not quite keen on adding these to the normal source
    port yet, so using a separate branch for this version for now.

--- code/qcommon/stv_version.h
+++ code/qcommon/stv_version.h
@@ -2,11 +2,11 @@
 #include "../win32/autoversion.h"
 
 #ifdef _DEBUG
-	#define	Q3_VERSION		"(debug) Speed Academy: v"VERSION_STRING_DOTTED
+	#define	Q3_VERSION		"(debug) Speed Academy (EY4): v"VERSION_STRING_DOTTED
 #elif defined FINAL_BUILD
-	#define	Q3_VERSION		"Speed Academy: v"VERSION_STRING_DOTTED
+	#define	Q3_VERSION		"Speed Academy (EY4): v"VERSION_STRING_DOTTED
 #else
-	#define	Q3_VERSION		"(internal) Speed Academy: v"VERSION_STRING_DOTTED
+	#define	Q3_VERSION		"(internal) Speed Academy (EY4): v"VERSION_STRING_DOTTED
 #endif
 // end
 
