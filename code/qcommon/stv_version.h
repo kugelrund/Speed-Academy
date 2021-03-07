b967fccaec7357fcc8d2f0fd3178a4805585f9ee
kugelrund <6236137+kugelrund@users.noreply.github.com>
2021-03-07 14:05:07 +0100

    Add special version info for segmented build

--- code/qcommon/stv_version.h
+++ code/qcommon/stv_version.h
@@ -2,11 +2,11 @@
 #include "../win32/autoversion.h"
 
 #ifdef _DEBUG
-	#define	Q3_VERSION		"(debug) Speed Academy: v"VERSION_STRING_DOTTED
+	#define	Q3_VERSION		"(debug) Speed Academy (segmented): v"VERSION_STRING_DOTTED
 #elif defined FINAL_BUILD
-	#define	Q3_VERSION		"Speed Academy: v"VERSION_STRING_DOTTED
+	#define	Q3_VERSION		"Speed Academy (segmented): v"VERSION_STRING_DOTTED
 #else
-	#define	Q3_VERSION		"(internal) Speed Academy: v"VERSION_STRING_DOTTED
+	#define	Q3_VERSION		"(internal) Speed Academy (segmented): v"VERSION_STRING_DOTTED
 #endif
 // end
 
